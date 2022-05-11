69c066827527280db0a85426e29485a785e3ff65
Devilish134 <kamilmusialik22@gmail.com>
2022-05-11 13:31:00 +0200

    + bootstrap Navbar & global styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Container,
+  Navbar,
+  Nav,
+} from 'react-bootstrap';
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Navbar bg='light' expand='lg'>
+      <Container>
+        <Navbar.Brand href='#'>
+          Waiter.app
+        </Navbar.Brand>
+        <Nav className='me-auto'>
+          <Nav.Link href='#home'>Home</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default App;
