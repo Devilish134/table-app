c24a244b24b6a44e6fe2388ab740aaa434d3a6c7
Devilish134 <kamilmusialik22@gmail.com>
2022-05-12 11:54:12 +0200

    + pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import {
+  Route,
+  Routes,
+} from 'react-router-dom';
 import Header from './components//views/Header/Header';
 import Footer from './components/views/Footer/Footer';
+import Home from './components/pages/Home/Home';
+import Table from './components/pages/Table/Table';
+import NotFound from './components/pages/NotFound/NotFound';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route
+          path='/table/:id'
+          element={<Table />}
+        />
+        <Route
+          path='*'
+          element={<NotFound />}
+        />
+      </Routes>
       <Footer />
     </>
   );
---
+++ src/components/pages/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+const NotFound = () => {
+  return (
+    <>
+      <h1 style={{ textAlign: 'center' }}>
+        404 NOT FOUND
+      </h1>
+      <p style={{ textAlign: 'center' }}>
+        page address is wrong or table don't
+        exist. Please verify and change
+      </p>
+    </>
+  );
+};
+
+export default NotFound;
